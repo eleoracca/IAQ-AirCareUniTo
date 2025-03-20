722d630513b8bced94e30d9f4f4007974779d710
eleoracca <28921698+eleoracca@users.noreply.github.com>
2025-03-20 15:15:18 +0100

    Added removing duplicates function and some comments and prints

--- moving.sh
+++ moving.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/sh
 set -e
 
+echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo " Script to remove duplicated files and move them to the sensors' folders."
+echo ""
+
+# Opening folder with data files
 cd scraping
 
+# Removing possible duplicated downloaded files
+echo " --- Removing duplicated files"
+find . -type f -regextype posix-extended -regex '.*\([0-9]+\)\.xls' -delete
+
+# Moving files to sensors' subfolders
+echo ""
+echo " --- Moving files to sensors' folders"
 for code in $(ls | grep .xls | cut -d _ -f 2 | sort | uniq) ; do
     mkdir -p $code && \
     mv *_${code}_*.xls $code/
 done
+
+echo ""
+echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo " Removal and file movement ended."
+echo " Thank you for your patience!"
+echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
